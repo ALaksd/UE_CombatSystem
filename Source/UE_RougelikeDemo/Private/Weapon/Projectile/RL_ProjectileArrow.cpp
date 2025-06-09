9e28e16528135349311cae2b742d35022be9d555
ASL <1151023266@qq.com>
2025-06-09 10:37:50 +0800

    Boss2_02

--- Source/UE_RougelikeDemo/Private/Weapon/Projectile/RL_ProjectileArrow.cpp
+++ Source/UE_RougelikeDemo/Private/Weapon/Projectile/RL_ProjectileArrow.cpp
@@ -4,10 +4,12 @@
 #include "Weapon/Projectile/RL_ProjectileArrow.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Components/SphereComponent.h"
+#include <AbilitySystemBlueprintLibrary.h>
 
 void ARL_ProjectileArrow::OnProjectileHit(UPrimitiveComponent* HitComponent, const FHitResult& HitResult)
 {
 	if (!HitComponent) return;
+
 
 	// 停止投射物移动
 	if (ProjectileCom)
@@ -29,31 +31,41 @@ void ARL_ProjectileArrow::OnProjectileHit(UPrimitiveComponent* HitComponent, con
 		EAttachmentRule::KeepWorld,
 		true
 	);
-
-	// 尝试附着到骨骼（如果是骨骼网格体）
-	if (HitResult.BoneName != NAME_None && HitComponent->IsA(USkeletalMeshComponent::StaticClass()))
+	
+	if (UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitResult.GetActor()))
 	{
-		AttachToComponent(HitComponent, AttachmentRules, HitResult.BoneName);
+		if (!ASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag("State.Bounceback")))
+		{
+			// 尝试附着到骨骼（如果是骨骼网格体）
+			if (HitResult.BoneName != NAME_None && HitComponent->IsA(USkeletalMeshComponent::StaticClass()))
+			{
+				AttachToComponent(HitComponent, AttachmentRules, HitResult.BoneName);
+			}
+			else
+			{
+				// 附着到静态网格体或其他组件
+				AttachToComponent(HitComponent, AttachmentRules);
+			}
+
+			// 调整位置和旋转以匹配命中点
+			FVector ArrowForward = GetActorForwardVector();
+			FVector HitNormal = HitResult.Normal;
+
+			// 计算箭矢应该旋转的角度，使其看起来是"钉"在表面上
+			FRotator NewRotation = FRotationMatrix::MakeFromX(-HitNormal).Rotator();
+			SetActorRotation(NewRotation);
+		}
+		else
+		{
+			Mesh->SetSimulatePhysics(true);
+			Mesh->SetEnableGravity(true);
+			Mesh->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
+			Mesh->AddImpulse(HitResult.Normal * 10000.f);
+		}
 	}
-	else
-	{
-		// 附着到静态网格体或其他组件
-		AttachToComponent(HitComponent, AttachmentRules);
-	}
-
-	// 调整位置和旋转以匹配命中点
-	FVector ArrowForward = GetActorForwardVector();
-	FVector HitNormal = HitResult.Normal;
-
-	// 计算箭矢应该旋转的角度，使其看起来是"钉"在表面上
-	FRotator NewRotation = FRotationMatrix::MakeFromX(-HitNormal).Rotator();
-	SetActorRotation(NewRotation);
-
-	// 微调位置，使箭矢看起来插入物体
-	SetActorLocation(HitResult.Location + HitNormal * 5.0f);
 
 	StopNiagara();
 
 	// 设置生命周期（可选），例如10秒后自动销毁
-	SetLifeSpan(10.0f);
+	SetLifeSpan(5.f);
 }
